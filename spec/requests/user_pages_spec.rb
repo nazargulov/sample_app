123305113e9ecd6836a21b60fee24ca931d4bf10
nazargulov <nazargulov.i@gmail.com>
2015-04-15 17:21:50 +0300

    added test for creating user

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe "UserPages", type: :request do
+RSpec.describe "User Pages", type: :request do
 
   subject { page }
 
@@ -12,10 +12,36 @@ RSpec.describe "UserPages", type: :request do
     it { should have_title(user.name) }
   end
 
-  describe "signup page" do
+
+  describe "signup" do
     before { visit signup_path }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    describe "page" do
+      it { should have_content('Sign up') }
+      it { should have_title(full_title('Sign up')) }
+    end
+
+    describe "create account" do
+      let(:submit) { "Create my account" }
+
+      describe "with blank information" do
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
+      end
+
+      describe "with valid information" do
+        before do
+          fill_in "Name", with: "Example User"
+          fill_in "Email", with: "user@example.com"
+          fill_in "Password", with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+        end
+
+        it "shoulde create a user" do
+          expect { click_button submit }.to change(User, :count).by(1)
+        end
+      end
+    end
   end
 end
