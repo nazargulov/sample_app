2aa8bb76cbe1b9acaa406bcc42747ec118cd56c8
nazargulov <nazargulov.i@gmail.com>
2015-04-17 12:13:47 +0300

    test for registration

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,9 +24,20 @@ RSpec.describe "User Pages", type: :request do
     describe "create account" do
       let(:submit) { "Create my account" }
 
-      describe "with blank information" do
+      describe "with invalid information" do
         it "should not create a user" do
           expect { click_button submit }.not_to change(User, :count)
+        end
+
+        describe "after submission" do
+          before { click_button submit }
+
+          it { should have_title('Sign up') }
+          it { should have_content('error') }
+          it { should have_content("Email can't be blank") }
+          it { should have_content("Password can't be blank") }
+          it { should have_content("Password is too short") }
+          it { should have_content("Email is invalid") }
         end
       end
 
@@ -40,6 +51,15 @@ RSpec.describe "User Pages", type: :request do
 
         it "shoulde create a user" do
           expect { click_button submit }.to change(User, :count).by(1)
+        end
+
+        describe "after saving the user" do
+          before { click_button submit }
+          let(:user) { User.find_by(email: "user@example.com") }
+
+          it { should have_title(user.name) }
+          it { should have_selector('div.alert.alert-success', text: 'Welcome')}
+
         end
       end
     end
